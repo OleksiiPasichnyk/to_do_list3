a82fe37602be5635bdb7deda390b8c305f430ddd
axel <axelpasechnik@gmail.com>
2020-08-16 23:06:41 -0400

    Updates

---
+++ precache-manifest.ddf0a95f38386b6f64da308f34a3d01b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affd72fa8a658dcb7f2bc91865a0d373",
+    "url": "/OleksiiPasichnyk/to_do_list3/index.html"
+  },
+  {
+    "revision": "cdf75a73b75408f89eb7",
+    "url": "/OleksiiPasichnyk/to_do_list3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "55b3291914523917acfe",
+    "url": "/OleksiiPasichnyk/to_do_list3/static/js/2.c3ebb0ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OleksiiPasichnyk/to_do_list3/static/js/2.c3ebb0ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf75a73b75408f89eb7",
+    "url": "/OleksiiPasichnyk/to_do_list3/static/js/main.d9471283.chunk.js"
+  },
+  {
+    "revision": "a077a021aa718cbaea21",
+    "url": "/OleksiiPasichnyk/to_do_list3/static/js/runtime-main.f75f3682.js"
+  }
+]);
